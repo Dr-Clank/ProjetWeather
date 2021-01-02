040e2dfac5b6c4b645a6ade7c3fa96528ac6aa25
Alkadios <lucas.delefortrie@free.fr>
2021-01-02 18:46:55 +0100

    Début création spinner grande ville 2.

--- Projet Weather/app/src/main/java/com/example/weatherapp/GrandeVilleActivity.java
+++ Projet Weather/app/src/main/java/com/example/weatherapp/GrandeVilleActivity.java
@@ -2,7 +2,9 @@ package com.example.weatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
@@ -10,16 +12,25 @@ import java.util.ArrayList;
 
 public class GrandeVilleActivity extends AppCompatActivity {
     private Spinner spinnerVilles;
+    private GrandeVilleActivityBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_grande_ville);
+        binding = GrandeVilleActivityBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view;
 
         initializeVilles();
     }
 
     private void initializeVilles() {
+        ArrayList<Ville> villes = new ArrayList<>();
+        villes.add(new Ville("Chat"));
+        villes.add(new Ville("Chien"));
 
+        ArrayAdapter<Ville> VilleArrayAdapter = new ArrayAdapter<Ville>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, villes);
+        VilleArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        binding.spinnerVilles.setAdapter(VilleArrayAdapter);
     }
 }
