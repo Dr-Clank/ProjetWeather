8dc923774c962031cc6afcef6d8d1206cef75327
Maxime OGIER <lolo.max@hotmail.com>
2021-01-02 17:02:58 +0100

    Mise en place des buttons on click listener du main activity

--- Projet Weather/app/src/main/java/com/example/weatherapp/MainActivity.java
+++ Projet Weather/app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.weatherapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonGrandeVille, buttonMonuments, buttonAgenda;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initbutton();
+
+        buttonMonuments.setOnClickListener((v) -> {
+
+        });
+
+        buttonGrandeVille.setOnClickListener((v) -> {
+
+        });
+
+        buttonAgenda.setOnClickListener((v)-> {
+
+        });
+    }
+
+    private void initbutton() {
+        buttonGrandeVille = findViewById(R.id.btGrandesVilles);
+        buttonAgenda = findViewById(R.id.btAgenda);
+        buttonMonuments = findViewById(R.id.btMonuments);
     }
 }
