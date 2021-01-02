4fff3879b13a3a962b7e111e40f9e69ec8a753f9
Alkadios <lucas.delefortrie@free.fr>
2021-01-02 18:19:43 +0100

    Création de l'intent du bouton ville et agenda..

--- Projet Weather/app/src/main/java/com/example/weatherapp/MainActivity.java
+++ Projet Weather/app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -2,10 +2,15 @@ package com.example.weatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentUris;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.view.View;
 import android.widget.Button;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,12 +32,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        buttonGrandeVille.setOnClickListener((v) -> {
-
+        buttonGrandeVille.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent listeGrandeVille = new Intent(getApplicationContext(), GrandeVilleActivity.class);
+                startActivity(listeGrandeVille);
+            }
         });
 
         buttonAgenda.setOnClickListener((v)-> {
-
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("content://com.android.calendar/time")));
         });
     }
 
