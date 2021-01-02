ee8842071fe1a54472ddd383e64b60aaa5b99305
Alkadios <lucas.delefortrie@free.fr>
2021-01-02 18:32:26 +0100

    Début création spinner grande ville.

--- Projet Weather/app/src/main/java/com/example/weatherapp/GrandeVilleActivity.java
+++ Projet Weather/app/src/main/java/com/example/weatherapp/GrandeVilleActivity.java
@@ -3,12 +3,23 @@ package com.example.weatherapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class GrandeVilleActivity extends AppCompatActivity {
+    private Spinner spinnerVilles;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grande_ville);
+
+        initializeVilles();
+    }
+
+    private void initializeVilles() {
+
     }
 }
